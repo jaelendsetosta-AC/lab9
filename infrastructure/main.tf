b015fade256ef1d99a6ee80e19998a99f8e4b81c
jaelendsetosta-AC <seto0046@algonquinlive.com>
2025-03-06 19:44:40 -0500

    Fixed syntax errors in main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,17 +4,15 @@ resource "azurerm_resource_group" "rg" {
   location = var.region
 }
 
+# Define the storage account
+resource "azurerm_storage_account" "storage" {
+  name                     = "${var.labelPrefix}storageaccountlab"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 
-# Define the virtual machine
-resource "azurerm_linux_virtual_machine" "webserver" {
-  name                  = "${var.labelPrefix}A05VM"
-  resource_group_name   = azurerm_resource_group.rg.name
-  location              = azurerm_resource_group.rg.location
-  network_interface_ids = [azurerm_network_interface.webserver.id]
-  size                  = "Standard_B1s"
-
-  os_disk {
-    name                 = "${var.labelPrefix}A05OSDisk"
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+  tags = {
+    environment = "lab"
   }
+}
---
+++ infrastructure/variables.tf
@@ -0,0 +1,9 @@
+variable "labelPrefix" {
+  type    = string
+  default = "seo"
+}
+
+variable "region" {
+  type    = string
+  default = "East US"
+}
