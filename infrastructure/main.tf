e55bddcd1b9eb09e1d9bfd617df558f6dbfaef68
jaelendsetosta-AC <seto0046@algonquinlive.com>
2025-03-06 19:31:20 -0500

    Test commit with no error

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "rg" {
 
 # Define the virtual machine
 resource "azurerm_linux_virtual_machine" "webserver" {
-    name                  = "${var.labelPrefix}A05VM"
+  name                  = "${var.labelPrefix}A05VM"
   resource_group_name   = azurerm_resource_group.rg.name
   location              = azurerm_resource_group.rg.location
   network_interface_ids = [azurerm_network_interface.webserver.id]
